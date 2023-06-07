e57db696132d4f1b84a240dc274c388ff8c1294f
Akshat103 <akshattamrakar103@gmail.com>
2023-06-07 09:25:18 +0530

    Added form

--- src/components/FormComponent.jsx
+++ src/components/FormComponent.jsx
@@ -1,39 +1,106 @@
-import { useState } from 'react';
+import { useState, useRef } from "react";
 
-const FormComponent = ({ onClose }) => {
-  const [formData, setFormData] = useState({
-    // Initialize form data here
-  });
+function FormComponent({ data }) {
+  const [isOpen, setIsOpen] = useState(false);
+  const initialRef = useRef();
+
+  const openForm = () => {
+    setIsOpen(true);
+  };
+
+  const closeForm = () => {
+    setIsOpen(false);
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    localStorage.setItem('formSubmit', JSON.stringify(formData));
-    onClose();
-  };
 
-  const handleChange = (e) => {
-    setFormData({
-      ...formData,
-      [e.target.name]: e.target.value,
-    });
+    const formData = {
+      numberOfTickets: e.target.elements["number-of-tickets"].value,
+      customerName: e.target.elements["customer-name"].value,
+      customerEmail: e.target.elements["customer-email"].value,
+      customerPhone: e.target.elements["customer-phone"].value
+    };
+
+    localStorage.setItem("formData", JSON.stringify(formData));
+
+    closeForm();
   };
 
   return (
-    <div className="modal">
-      <form onSubmit={handleSubmit}>
+    <>
+      <button onClick={openForm}>Book Now</button>
 
-        <input
-          type="text"
-          name="name"
-          value={formData.name || ''}
-          onChange={handleChange}
-          placeholder="Name"
-        />
-
-        <button type="submit">Submit</button>
-      </form>
-    </div>
+      {isOpen && (
+        <div className="modal-overlay">
+          <div className="modal">
+            <span className="close" onClick={closeForm}>
+              &times;
+            </span>
+            <form onSubmit={handleSubmit}>
+              <p className="name">{data.name}</p>
+              <div className="details">
+                <div className="innerdiv">
+                  <p>Premiered: {data.premiered}</p>
+                  {data.schedule.days.length !== 0 && (
+                    <p>
+                      {data.schedule.days.join(", ")} at {data.schedule.time}
+                    </p>
+                  )}
+                </div>
+              </div>
+              <div className="form-group">
+                <input
+                  type="number"
+                  className="form-control"
+                  id="number-of-tickets"
+                  name="number-of-tickets"
+                  placeholder="Tickets"
+                  required
+                />
+              </div>
+              <div className="form-group">
+                <input
+                  type="text"
+                  className="form-control"
+                  id="customer-name"
+                  name="customer-name"
+                  placeholder="Name"
+                  required
+                />
+              </div>
+              <div className="form-group">
+                <input
+                  type="email"
+                  className="form-control"
+                  id="customer-email"
+                  name="customer-email"
+                  placeholder="Email"
+                  required
+                />
+              </div>
+              <div className="form-group">
+                <input
+                  type="tel"
+                  className="form-control"
+                  id="customer-phone"
+                  name="customer-phone"
+                  placeholder="Phone Number"
+                  required
+                />
+              </div>
+              <div>
+                <button type="submit">Book</button>
+                <button type="button" onClick={closeForm}>
+                  Cancel
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      )}
+    </>
   );
-};
+}
 
 export default FormComponent;
--- src/components/OneShow.jsx
+++ src/components/OneShow.jsx
@@ -1,71 +1,73 @@
 import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom"
+import { useParams } from "react-router-dom";
 import FormComponent from "./FormComponent";
 
+
 function OneShow() {
+  const [show, setShow] = useState([]);
+  const param = useParams();
 
-    const [show, setShow] = useState([]);
-    const param = useParams();
+  const [data, setData] = useState([]);
 
-    const [data, setData] = useState([]);
+  useEffect(() => {
+    const items = JSON.parse(localStorage.getItem("data"));
+    if (items) {
+      setData(items);
+    }
+  }, []);
 
-    useEffect(() => {
-        const items = JSON.parse(localStorage.getItem('data'));
-        if (items) {
-            setData(items);
-        }
-    }, []);
+  useEffect(() => {
+    if (data.length > 0) {
+      let searchResults = data.filter((item) => item.show.id == param.id);
+      searchResults = searchResults[0].show;
+      setShow(searchResults);
+    }
+  }, [data, param.id]);
 
-    useEffect(() => {
-        if (data.length > 0) {
-            let searchResults = data.filter(item => item.show.id == param.id);
-            searchResults = searchResults[0].show;
-            setShow(searchResults);
-        }
-    }, [data, param.id]);
+  var size = Object.keys(show).length;
+  if (!size) return <h3>Loading...</h3>;
 
-    var size = Object.keys(show).length;
-    if (!size) return (
-        <h3>Loading...</h3>
-    );
-
-    return (
+  return (
+    <>
+      {size > 0 ? (
         <>
-            {
-                size > 0 ? <>
-                    <h3 className="show-name">{show.name}</h3>
-                    <div className="show">
-                        <div className="img">
-                            <img src={show.image.original} alt={show.title} />
-                        </div>
-                        <div className="show-details">
-  <div dangerouslySetInnerHTML={{ __html: show.summary }} className="summary" />
-  <div className="details">
-    <div className="innerdiv">
-      <p>Type: {show.type}</p>
-      <p>Language: {show.language}</p>
-      <p>Genres: {show.genres.join(', ')}</p>
-    </div>
-    <div className="innerdiv">
-      <p>Premiered: {show.premiered}</p>
-      {show.schedule.days.length !== 0 && (
-        <p>{show.schedule.days.join(', ')} at {show.schedule.time}</p>
-      )}
-      <p>Rating: {show.rating.average}</p>
-    </div>
-  </div>
-</div>
-
-
-                        <button >Book Now</button>
-                    </div>
-                </>
-                    :
-                    <h1>SHOW NOT FOUND</h1>
-            }
-
+          <h3 className="show-name">{show.name}</h3>
+          <div className="show">
+            <div className="img">
+              <img src={show.image.original} alt={show.title} />
+            </div>
+            <div className="show-details">
+              <div
+                dangerouslySetInnerHTML={{ __html: show.summary }}
+                className="summary"
+              />
+              <div className="details">
+                <div className="innerdiv">
+                  <p>Type: {show.type}</p>
+                  <p>Language: {show.language}</p>
+                  <p>Genres: {show.genres.join(", ")}</p>
+                </div>
+                <div className="innerdiv">
+                  <p>Premiered: {show.premiered}</p>
+                  {show.schedule.days.length !== 0 && (
+                    <p>
+                      {show.schedule.days.join(", ")} at {show.schedule.time}
+                    </p>
+                  )}
+                  <p>Rating: {show.rating.average}</p>
+                </div>
+              </div>
+            </div>
+            <div className="form">
+                <FormComponent data={show} />
+            </div>
+          </div>
         </>
-    );
+      ) : (
+        <h1>SHOW NOT FOUND</h1>
+      )}
+    </>
+  );
 }
 
 export default OneShow;
